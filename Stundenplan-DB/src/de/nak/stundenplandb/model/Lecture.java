dec19e12e5ee83197558fc2783a0d6c4ff3dd16c
Lars <lale87@users.noreply.github.com>
2014-10-25 13:21:19 +0200

    Klassen (Gerüst) für die konkreten Veranstaltungen angelegt

---
+++ Stundenplan-DB/src/de/nak/stundenplandb/model/Lecture.java
@@ -0,0 +1,11 @@
+package de.nak.stundenplandb.model;
+
+/**
+ * Lecture Entity.
+ * 
+ * @author Lars Lembke
+ *
+ */
+public class Lecture extends Meeting {
+	// TODO nyi
+}
---
+++ Stundenplan-DB/src/de/nak/stundenplandb/model/Seminar.java
@@ -0,0 +1,11 @@
+package de.nak.stundenplandb.model;
+
+/**
+ * Seminar Entity
+ * 
+ * @author Lars Lembke
+ *
+ */
+public class Seminar extends Meeting {
+	// TODO nyi
+}
