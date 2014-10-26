9829885c4e8b5d0fda44c1ba13674f195548bb02
Lars <lale87@users.noreply.github.com>
2014-10-26 13:17:05 +0100

    MeetingService implementiert

--- Stundenplan-DB/src/de/nak/stundenplandb/service/MeetingService.java
+++ Stundenplan-DB/src/de/nak/stundenplandb/service/MeetingService.java
@@ -1,6 +1,9 @@
 package de.nak.stundenplandb.service;
 
-import de.nak.stundenplandb.model.Meeting;
+import de.nak.stundenplandb.model.Elective;
+import de.nak.stundenplandb.model.Exam;
+import de.nak.stundenplandb.model.Lecture;
+import de.nak.stundenplandb.model.Seminar;
 
 /**
  * Schnittstelle für den MeetingService
@@ -10,18 +13,64 @@ import de.nak.stundenplandb.model.Meeting;
  */
 public interface MeetingService {
 	/**
-	 * Creates or updates a Meeting.
+	 * Creates or updates an Exam.
 	 *
-	 * @param meeting
-	 *            The meeting.
+	 * @param exam
+	 *            The exam.
 	 */
-	void saveMeeting(Meeting meeting);
+	void saveExam(Exam exam);
 
 	/**
-	 * deletes a Meeting.
+	 * deletes an Exam.
 	 *
-	 * @param meeting
-	 *            The meeting.
+	 * @param exam
+	 *            The exam.
 	 */
-	void deleteMeeting(Meeting meeting);
+	void deleteExam(Exam exam);
+	
+	/**
+	 * Creates or updates a Lecture.
+	 *
+	 * @param Lecture
+	 *            The lecture.
+	 */
+	void saveLecture(Lecture lecture);
+	
+	/**
+	 * deletes a Lecture.
+	 *
+	 * @param lecture
+	 *            The lecture.
+	 */
+	void deleteLecture(Lecture lecture);
+	/**
+	 * Creates or updates a Seminar.
+	 *
+	 * @param seminar
+	 *            The seminar.
+	 */
+	void saveSeminar(Seminar seminar);
+	
+	/**
+	 * deletes a Seminar.
+	 *
+	 * @param Seminar
+	 *            The seminar.
+	 */
+	void deleteSeminar(Seminar seminar);
+	/**
+	 * Creates or updates an Elective.
+	 *
+	 * @param elective
+	 *            The elective.
+	 */
+	void saveElective(Elective elective);
+	
+	/**
+	 * deletes an Elective.
+	 *
+	 * @param elective
+	 *            The elective.
+	 */
+	void deleteElective(Elective elective);
 }
