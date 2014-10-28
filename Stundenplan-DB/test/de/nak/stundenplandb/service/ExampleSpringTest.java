0fe4b0b945859dae6bb81b0b8422e524e428756e
Fabian <fabian.kolossa@gmail.com>
2014-10-28 10:24:36 +0100

    Spring-Konfiguration aufgeteilt (DB & Beans), jQuery-Plugin hinzugefügt

--- Stundenplan-DB/test/de/nak/stundenplandb/service/ExampleSpringTest.java
+++ Stundenplan-DB/test/de/nak/stundenplandb/service/ExampleSpringTest.java
@@ -9,16 +9,30 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.nak.stundenplandb.dao.RoomDAO;
+import de.nak.stundenplandb.model.ERoomType;
+import de.nak.stundenplandb.model.Room;
 
-// Spring configuration
 @RunWith(SpringJUnit4ClassRunner.class)
-// Load spring context from classpath
-@ContextConfiguration("/spring-config.xml")
+@ContextConfiguration(locations = {
+		"/spring-test-db-config.xml",
+		"/spring-beans-config.xml"})
 public class ExampleSpringTest {
 
 	@Autowired
 	private RoomDAO roomDao;
 
+	@Test
+	public void createNewRoomTest() {
+		Room room = new Room();
+		room.setRoomType(ERoomType.CLASSROOM);
+		room.setBuilding("A");
+		room.setRoomNumber(101);
+		room.setSeats(34);
+		room.setChangingTime(25);
+		
+		roomDao.save(room);
+	}
+	
 	@Test
 	public void isRoomDaoSetBySpringTest() {
 		assertNotNull("RoomDao nicht gesetzt.", roomDao);
