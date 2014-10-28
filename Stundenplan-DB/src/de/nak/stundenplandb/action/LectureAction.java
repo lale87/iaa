76e8632d35afb36bf590751d7ae5368052344617
aroever2 <aroever2@users.noreply.github.com>
2014-10-28 12:22:36 +0100

    LectureAction erbt jetzt von MeetingAction.

--- Stundenplan-DB/src/de/nak/stundenplandb/action/LectureAction.java
+++ Stundenplan-DB/src/de/nak/stundenplandb/action/LectureAction.java
@@ -3,26 +3,14 @@
  */
 package de.nak.stundenplandb.action;
 
-import java.util.Date;
-import java.util.List;
-
-import com.opensymphony.xwork2.ActionSupport;
-
 import de.nak.stundenplandb.model.Lecture;
-import de.nak.stundenplandb.model.Lecturer;
-import de.nak.stundenplandb.model.Room;
-import de.nak.stundenplandb.model.StudentGroup;
-import de.nak.stundenplandb.service.LecturerService;
-import de.nak.stundenplandb.service.MeetingService;
-import de.nak.stundenplandb.service.RoomService;
-import de.nak.stundenplandb.service.StudentGroupService;
 
 /**
  * The Class LectureAction.
  *
  * @author Arne Roever
  */
-public class LectureAction extends ActionSupport {
+public class LectureAction extends MeetingAction {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 5470788664512099858L;
@@ -30,109 +18,17 @@ public class LectureAction extends ActionSupport {
 	/** The lecture. */
 	private Lecture lecture;
 	
-	/** The lecturer service. */
-	private LecturerService lecturerService;
-	
-	/** The room service. */
-	private RoomService roomService;
-	
-	/** The student group service. */
-	private StudentGroupService studentGroupService;
-	
-	/** The start date. */
-	private Date startDate;
-	
-	/** The end date. */
-	private Date endDate;
-	
-	/** The number of appointments. */
-	private Integer numberOfAppointments;
-	
-	/** The meeting service. */
-	private MeetingService meetingService;
-	
-	/**
-	 * Gets all lecturers.
-	 *
-	 * @return the all lecturers
-	 */
-	public List<Lecturer> getAllLecturers(){
-		return lecturerService.loadAllLecturers();
-	}
-	
-	/**
-	 * Gets all rooms.
-	 *
-	 * @return the all rooms
-	 */
-	public List<Room> getAllRooms(){
-		return roomService.loadAllRooms();
-	}
-	
-	/**
-	 * Gets all student groups.
-	 *
-	 * @return the student groups
-	 */
-	public List<StudentGroup> getAllStudentGroups(){
-		return studentGroupService.loadAllStudentGroups();
-	}
-	
 	public String save(){
-		meetingService.saveLecture(lecture, numberOfAppointments, startDate, endDate);
+		getMeetingService().saveLecture(lecture, getNumberOfAppointments(), getStartDate(), getEndDate());
 		//meetingService.saveLecture(lecturerId,roomIds,studentGroupId,lectureName,startDate,endDate, numberOfAppointments);
 		return SUCCESS;
 	}
 
-	public void setLecturerService(LecturerService lecturerService) {
-		this.lecturerService = lecturerService;
-	}
-
-	public void setRoomService(RoomService roomService) {
-		this.roomService = roomService;
-	}
-
-	public void setStudentGroupService(StudentGroupService studentGroupService) {
-		this.studentGroupService = studentGroupService;
-	}	
-
-	public Date getStartDate() {
-		return startDate;
-	}
-
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-
-	public Integer getNumberOfAppointments() {
-		return numberOfAppointments;
-	}
-
-	public void setNumberOfAppointments(Integer numberOfAppointments) {
-		this.numberOfAppointments = numberOfAppointments;
-	}
-
-	public Date getEndDate() {
-		return endDate;
-	}
-
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	
 	public Lecture getLecture() {
 		return lecture;
 	}
 
 	public void setLecture(Lecture lecture) {
 		this.lecture = lecture;
-	}
-
-	public void setMeetingService(MeetingService meetingService) {
-		this.meetingService = meetingService;
-	}
-	
-	
-	
-	
+	}	
 }
